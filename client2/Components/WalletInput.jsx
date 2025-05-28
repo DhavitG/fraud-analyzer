5c2d2aa0ef3faa68dfea6286c0b8a004a3d3c5cc
DhavitG <gdhavit@gmail.com>
2025-05-29 00:26:56 +0530

    Frontend revamp

--- client2/Components/WalletInput.jsx
+++ client2/Components/WalletInput.jsx
@@ -7,8 +7,12 @@ export default function WalletInput() {
           placeholder="Enter wallet address"
           className="px-4 py-2 border border-gray-300 rounded w-96 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button className="px-4 py-2 bg-black text-white rounded shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-black">
+        <button className="px-4 py-2 bg-black text-white rounded shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 cursor-pointer">
           Scan
+        </button>
+
+        <button className="px-4 py-2 bg-blue-600 text-white rounded shadow-sm hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 cursor-pointer">
+          Report
         </button>
       </div>
     </div>
