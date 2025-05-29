c0353940182a500c2eedda89bdf6a878e7ffc9db
DhavitG <gdhavit@gmail.com>
2025-05-30 05:14:22 +0530

    Sidebar css

--- client/Components/Sidebar.jsx
+++ client/Components/Sidebar.jsx
@@ -26,10 +26,10 @@ export default function Sidebar() {
 
         <button
           type="button"
-          className="flex items-center space-x-3 px-3 py-2 rounded-lg hover:bg-white hover:shadow cursor-pointer transition"
+          className="flex items-center space-x-3 px-3 py-2 rounded-lg bg-white shadow font-medium text-stone-800"
         >
           <Search className="w-5 h-5" strokeWidth={1.5} />
-          <span className="font-medium text-stone-800">Scan Wallet</span>
+          <span>Scan Wallet</span>
         </button>
 
         <button
